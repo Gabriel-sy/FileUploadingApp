fc74ad2818b8b3181a53de4bd0d04ce8445cb8eb
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-07-06 13:17:34 -0300

    fix: refactor services

--- drive-front/src/app/services/upload/folder.service.ts
+++ drive-front/src/app/services/upload/folder.service.ts
@@ -2,12 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FolderClass } from '../../domain/Folder';
 import { FileClass } from '../../domain/File';
-import { delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FolderService {
+
+  private readonly API = 'http://localhost:8080/'
 
   constructor(private http: HttpClient) { }
 
@@ -15,25 +16,25 @@ export class FolderService {
     let header = new HttpHeaders();
     header = header.set('Content-Type', 'application/json; charset=utf-8')
     var json = JSON.stringify({ name: folderName })
-    return this.http.post<FolderClass>('http://localhost:8080/folder/create/' + userId, json, { headers: header })
+    return this.http.post<FolderClass>(this.API + 'folder/create/' + userId, json, { headers: header })
   }
 
   findAllFolders(userId: string) {
-    return this.http.get<FolderClass[]>('http://localhost:8080/folder/findall/' + userId)
+    return this.http.get<FolderClass[]>(this.API + 'folder/findall/' + userId)
   }
 
   findAllFilesByFolderId(id: string) {
-    return this.http.get<FileClass[]>('http://localhost:8080/file/get/folderid/' + id)
+    return this.http.get<FileClass[]>(this.API + 'file/get/folderid/' + id)
   }
 
   setFolderSize(folderId: number, size: number) {
     let header = new HttpHeaders();
     header = header.set('Content-Type', 'application/json; charset=utf-8')
     var json = JSON.stringify({ folderId: folderId, size: size })
-    return this.http.post('http://localhost:8080/folder/save/foldersize', json, { headers: header })
+    return this.http.post(this.API + 'folder/save/foldersize', json, { headers: header })
   }
 
   deleteFolder(folderId: string) {
-    return this.http.delete('http://localhost:8080/folder/delete/' + folderId);
+    return this.http.delete(this.API + 'folder/delete/' + folderId);
   }
 }
